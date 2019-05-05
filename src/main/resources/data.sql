e20f8f517d36f0d4baf2c1fb00de3b5937d026a0
marianoiglesiasmarchese <marianoiglesiasmarchese@gmail.com>
2019-05-05 18:11:57 -0300

    feature(): init data for h2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into recipe (id, description, image, link, name)
+values(101,'Stranger description 1', '5b73616473646164736164735d', 'http://www.faqs.org/rfcs/rfc3987.html', 'Stranger 1');
+
+insert into recipe (id, description, image, link, name)
+values(102,'Stranger description 2', '5b73616473646164736164735d', 'http://www.faqs.org/rfcs/rfc3987.html', 'Stranger 2');
+
+insert into recipe (id, description, image, link, name)
+values(103,'Stranger description 3', '5b73616473646164736164735d', 'http://www.faqs.org/rfcs/rfc3987.html', 'Stranger 3');
+
+insert into recipe (id, description, image, link, name)
+values(104,'Stranger description 4', '5b73616473646164736164735d', 'http://www.faqs.org/rfcs/rfc3987.html', 'Stranger 4');
+
+insert into recipe (id, description, image, link, name)
+values(105,'Stranger description 5', '5b73616473646164736164735d', 'http://www.faqs.org/rfcs/rfc3987.html', 'Stranger 5');
